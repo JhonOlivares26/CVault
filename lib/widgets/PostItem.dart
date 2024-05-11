92816eeeca48be57299af3a67a0515e9acc95bd6
JuanGomez-14 <jmgomezc14@gmail.com>
2024-05-11 02:35:19 -0500

    Bug Fix descripcion larga bottonbottom overflowed, ajustes de tamaño, espacios, se añade una pagina nueva para mostrar el post completo al darle click

--- lib/widgets/PostItem.dart
+++ lib/widgets/PostItem.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:cvault/views/pages/PostDetails.dart';
 
 class PostItem extends StatefulWidget {
   final DocumentSnapshot post;
@@ -17,40 +18,84 @@ class _PostItemState extends State<PostItem> {
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic> data = widget.post.data() as Map<String, dynamic>;
-    String docId = widget.post.id;
 
-    return ListTile(
-      leading: Image.network(data['imageUrl']),
-      title: Text(data['title']),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(data['description']),
-          Text('${DateFormat('dd/MM/yyyy').format((data['timestamp'] as Timestamp).toDate())}'),
-        ],
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          GestureDetector(
-            onTap: () async {
-              // Actualiza el estado
-              setState(() {
-                isLiked = !isLiked;
-              });
-
-              // Actualiza Firestore
-              await FirebaseFirestore.instance.collection('posts').doc(docId).update({
-                'likes': FieldValue.increment(isLiked ? 1 : -1),
-              });
-            },
-            child: Icon(
-              Icons.favorite,
-              color: isLiked ? Colors.red : Colors.grey,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => PostDetails(
+              title: data['title'],
+              imageUrl: data['imageUrl'],
+              description: data['description'],
+              likes: data['likes'],
             ),
           ),
-          Text('${data['likes']}'),
-        ],
+        );
+      },
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 100,
+                child: Image.network(data['imageUrl'], fit: BoxFit.cover),
+              ),
+              SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      data['title'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      data['description'],
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      '${DateFormat('dd/MM/yyyy').format((data['timestamp'] as Timestamp).toDate())}',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  setState(() {
+                    isLiked = !isLiked;
+                  });
+                  await FirebaseFirestore.instance
+                      .collection('posts')
+                      .doc(widget.post.id)
+                      .update({
+                    'likes': FieldValue.increment(isLiked ? 1 : -1),
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Icon(
+                      Icons.favorite,
+                      color: isLiked ? Colors.red : Colors.grey,
+                    ),
+                    SizedBox(height: 4.0),
+                    Text(
+                      '${data['likes']}',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
