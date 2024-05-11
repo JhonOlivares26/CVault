41654b0cc14de86c1c770ea82e27fb867ce313da
JuanGomez-14 <jmgomezc14@gmail.com>
2024-05-11 01:29:14 -0500

    Ajustes en los inputs, editar la imagen de perfil se añade un boton para hacerlo más claro al usuario, se situa en un mejor lugar el boton eliminar y se cambia por un icono

--- lib/views/pages/ProfilePage.dart
+++ lib/views/pages/ProfilePage.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -21,12 +20,19 @@ class _ProfilePageState extends State<ProfilePage> {
   String? name;
   String? userType;
   String? photo;
+  bool _isLoading = false;
 
   Future<void> pickImage() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
-      final firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+      setState(() {
+        _isLoading = true;
+      });
+
+      final firebase_storage.Reference ref = firebase_storage
+          .FirebaseStorage.instance
           .ref()
           .child('userProfile')
           .child('${user!.uid}.jpg');
@@ -36,11 +42,16 @@ class _ProfilePageState extends State<ProfilePage> {
       photo = await ref.getDownloadURL();
 
       // Actualiza el campo 'photo' en Firestore
-      await FirebaseFirestore.instance.collection('users').doc(user!.uid).update({
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user!.uid)
+          .update({
         'photo': photo,
       });
 
-      setState(() {}); // Actualiza la UI
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -48,12 +59,24 @@ class _ProfilePageState extends State<ProfilePage> {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
 
+      setState(() {
+        _isLoading = true;
+      });
+
       // Actualiza solo el campo 'name' en Firestore
-      await FirebaseFirestore.instance.collection('users').doc(user!.uid).update({
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user!.uid)
+          .update({
         'name': name,
       });
 
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Perfil actualizado')));
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Perfil actualizado')));
+
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
@@ -64,77 +87,130 @@ class _ProfilePageState extends State<ProfilePage> {
         title: Text('Perfil'),
       ),
       body: StreamBuilder<DocumentSnapshot>(
-        stream: FirebaseFirestore.instance.collection('users').doc(user!.uid).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(user!.uid)
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
           if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           }
 
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Cargando...');
+            return Center(child: CircularProgressIndicator());
           }
 
-          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-          photo = data['photo']; // Asigna el valor de 'photo' a partir de los datos del snapshot
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          photo = data[
+              'photo']; // Asigna el valor de 'photo' a partir de los datos del snapshot
 
           return Form(
             key: _formKey,
-            child: Column(
-              children: <Widget>[
-                GestureDetector(
-                  onTap: pickImage,
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: photo != null ? NetworkImage(photo!) : null,
-                    child: photo == null ? Icon(Icons.camera_alt, size: 50) : null,
-                  ),
-                ),
-                TextFormField(
-                  initialValue: data['name'],
-                  decoration: InputDecoration(labelText: 'Nombre'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor ingresa tu nombre';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    name = value;
-                  },
-                ),
-                TextFormField(
-                  initialValue: data['email'],
-                  decoration: InputDecoration(labelText: 'Email'),
-                  enabled: false, // Deshabilita el campo de email para que no se pueda modificar
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: saveProfile,
-                  child: Text('Guardar'),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    await showDialog(
-                      context: context,
-                      builder: (BuildContext context) => ConfirmationDialog(
-                        title: 'Confirmar eliminación',
-                        content: '¿Estás seguro de que quieres eliminar tu cuenta?',
-                        onConfirm: () async {
-
-                          String userId = auth.FirebaseAuth.instance.currentUser!.uid; // Obtén el ID del usuario actual
-                          await FirebaseService().deleteAccount(userId); // Pasa el ID del usuario a deleteAccount
-                        },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage:
+                            photo != null ? NetworkImage(photo!) : null,
+                        child: photo == null
+                            ? Icon(Icons.camera_alt, size: 50)
+                            : null,
                       ),
-                    );
-                    await showAlert(context, 'Cuenta eliminada con éxito', 'no te extrañaremos');
-                    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => LoginPage()), (Route<dynamic> route) => false);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red, // Color de fondo del botón
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: pickImage,
+                        ),
+                      ),
+                      _isLoading ? CircularProgressIndicator() : Container(),
+                    ],
                   ),
-                  child: const Text( 'Eliminar cuenta', style: TextStyle(color: Colors.white)), // Color del texto del botón
-                ),
-              ],
+                  SizedBox(height: 20),
+                  TextFormField(
+                    initialValue: data['name'],
+                    decoration: InputDecoration(labelText: 'Nombre'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa tu nombre';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      name = value;
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    initialValue: data['email'],
+                    decoration: InputDecoration(labelText: 'Email'),
+                    enabled: false,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: saveProfile,
+                    child: Text('Guardar'),
+                  ),
+                  Spacer(),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Colors.red,
+                      onPressed: () async {
+                        bool confirm = await showDialog(
+                          context: context,
+                          builder: (BuildContext context) => ConfirmationDialog(
+                            title: 'Confirmar eliminación',
+                            content:
+                                '¿Estás seguro de que quieres eliminar tu cuenta?',
+                            onConfirm: () async {
+                              setState(() {
+                                _isLoading = true;
+                              });
+
+                              String userId =
+                                  auth.FirebaseAuth.instance.currentUser!.uid;
+                              await FirebaseService().deleteAccount(userId);
+
+                              setState(() {
+                                _isLoading = false;
+                              });
+
+                              await showAlert(
+                                context,
+                                'Cuenta eliminada con éxito',
+                                'No te olvidaremos',
+                              );
+                              Navigator.of(context).pushAndRemoveUntil(
+                                MaterialPageRoute(
+                                  builder: (context) => LoginPage(),
+                                ),
+                                (Route<dynamic> route) => false,
+                              );
+                            },
+                          ),
+                        );
+
+                        // Para evitar un error si el usuario cierra el diálogo sin confirmar
+                        if (confirm == null) {
+                          return;
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
