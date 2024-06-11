3e181df04d7db2ce43d4ff9a2ffa60e10440f860
JuanGomez-14 <jmgomezc14@gmail.com>
2024-06-10 19:09:21 -0500

    Ajustes visuales en la informacion detallada de una oferta de empleo

--- lib/views/pages/JobDetails.dart
+++ lib/views/pages/JobDetails.dart
@@ -18,6 +18,7 @@ class JobDetailsPage extends StatefulWidget {
 class _JobDetailsPageState extends State<JobDetailsPage> {
   bool hasApplied = false;
   String userType = '';
+  bool isLoading = true;
 
   @override
   void initState() {
@@ -28,10 +29,14 @@ class _JobDetailsPageState extends State<JobDetailsPage> {
   Future<void> _getUserType() async {
     final currentUser = auth.FirebaseAuth.instance.currentUser;
     if (currentUser != null) {
-      final userDoc = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+      final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser.uid)
+          .get();
       setState(() {
         userType = userDoc.data()?['userType'] ?? '';
         hasApplied = widget.job.applicants.contains(currentUser.uid);
+        isLoading = false;
       });
     }
   }
@@ -42,64 +47,119 @@ class _JobDetailsPageState extends State<JobDetailsPage> {
       appBar: AppBar(
         title: Text(widget.job.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(widget.job.title, style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 10),
-            Text('Company: ${widget.job.companyName}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Location: ${widget.job.location}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Salary: ${widget.job.salary}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Modality: ${widget.job.modality}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Acerca del empleo: ${widget.job.description}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 20),
-            Text('Solicitantes: ${widget.job.applicants.length}'),
-            const SizedBox(height: 20),
-            if (userType == 'Persona')
-              ElevatedButton(
-                onPressed: hasApplied ? null : () async {
-                  final currentUser = auth.FirebaseAuth.instance.currentUser;
-                  if (currentUser != null) {
-                    if (!widget.job.applicants.contains(currentUser.uid)) {
-                      final userDoc = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
-                      final userPdf = userDoc.data()?['userPdf'] ?? '';
-                      if (userPdf.isEmpty) {
-                        showAlert(
-                          context,
-                          'Error',
-                          'Debes cargar tu hoja de vida antes de postularte a un trabajo.',
-                        );
-                      } else {
-                        setState(() {
-                          widget.job.applicants.add(currentUser.uid);
-                          hasApplied = true;
-                        });
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    widget.job.title,
+                    style: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Card(
+                    child: ListTile(
+                      leading: Icon(Icons.business),
+                      title: Text('Compañia'),
+                      subtitle: Text(widget.job.companyName),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: Icon(Icons.location_on),
+                      title: Text('Lugar'),
+                      subtitle: Text(widget.job.location),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: Icon(Icons.monetization_on),
+                      title: Text('Salario'),
+                      subtitle: Text(widget.job.salary),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: Icon(Icons.work),
+                      title: Text('Modalidad'),
+                      subtitle: Text(widget.job.modality),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Acerca del empleo:',
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    widget.job.description,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 20),
+                  Divider(),
+                  Text('Solicitantes: ${widget.job.applicants.length}'),
+                  const SizedBox(height: 20),
+                  if (userType == 'Persona')
+                    ElevatedButton.icon(
+                      onPressed: hasApplied
+                          ? null
+                          : () async {
+                              final currentUser =
+                                  auth.FirebaseAuth.instance.currentUser;
+                              if (currentUser != null) {
+                                if (!widget.job.applicants
+                                    .contains(currentUser.uid)) {
+                                  final userDoc = await FirebaseFirestore
+                                      .instance
+                                      .collection('users')
+                                      .doc(currentUser.uid)
+                                      .get();
+                                  final userPdf =
+                                      userDoc.data()?['userPdf'] ?? '';
+                                  if (userPdf.isEmpty) {
+                                    showAlert(
+                                      context,
+                                      'Error',
+                                      'Debes cargar tu hoja de vida antes de postularte a un trabajo.',
+                                    );
+                                  } else {
+                                    setState(() {
+                                      widget.job.applicants
+                                          .add(currentUser.uid);
+                                      hasApplied = true;
+                                    });
 
-                        FirebaseFirestore.instance.collection('jobs').doc(widget.job.id).update({
-                          'applicants': FieldValue.arrayUnion([currentUser.uid])
-                        });
-                        showAlert(
-                          context,
-                          'Postulación exitosa',
-                          'Has solicitado el empleo exitosamente.',
-                        );
-                      }
-                    }
-                  }
-                },
-                child: const Text('Solicitar empleo'),
+                                    FirebaseFirestore.instance
+                                        .collection('jobs')
+                                        .doc(widget.job.id)
+                                        .update({
+                                      'applicants': FieldValue.arrayUnion(
+                                          [currentUser.uid])
+                                    });
+                                    showAlert(
+                                      context,
+                                      'Postulación exitosa',
+                                      'Has solicitado el empleo exitosamente.',
+                                    );
+                                  }
+                                }
+                              }
+                            },
+                      icon: Icon(Icons.send),
+                      label: Text('Solicitar empleo'),
+                    ),
+                  if (hasApplied)
+                    const Text(
+                      'Ya estás postulado para este empleo',
+                      style: TextStyle(fontSize: 18, color: Colors.green),
+                    ),
+                ],
               ),
-            if (hasApplied) // Muestra un mensaje si el usuario ha solicitado el empleo
-              const Text('Ya estás postulado para este empleo', style: TextStyle(fontSize: 18, color: Colors.green)),
-          ],
-        ),
-      ),
+            ),
     );
   }
 }
